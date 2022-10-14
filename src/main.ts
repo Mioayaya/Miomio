da3ca07502ee2afcc155f156dd4eae960db5ba14
Mioaya <1530020617@qq.com>
2022-10-14 12:50:52 +0800

    pinia

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './route'
+import store from './store'
 
 const app = createApp(App);
 
-app.use(router).mount('#app');
+app.use(store)
+  .use(router)
+  .mount('#app');
--- src/views/test/c-views/TestTwoView.vue
+++ src/views/test/c-views/TestTwoView.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
+import { testStore } from '../../../store/pinia/test';
+
+const store = testStore();
+const { count, name } = store;
 
 </script>
 
 <template>
   <div class="test2">
     <h2>test2</h2>
+    <h2>{{name}}</h2>
+    <h2>{{count}}</h2>
   </div>
 </template>
 
